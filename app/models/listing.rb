8453dbcdcc796804f322f01338de4aab5fadeff8
rikifujihara <rikifujihara@gmail.com>
2022-06-07 20:38:28 +1000

    added condition enum

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,11 @@ class Listing < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_rich_text :description
+
+  enum condition: {
+    extremely_worn: 1,
+    slightly_worn: 2,
+    good_condition: 3,
+    excellent_condition: 4
+  }
 end
