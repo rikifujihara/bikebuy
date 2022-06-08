c162ed01b91fa0f2a5dbb54773babca4d36cb851
rikifujihara <rikifujihara@gmail.com>
2022-06-08 12:11:48 +1000

    added listings index and root to navbar

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,6 +1,6 @@
 module ListingsHelper
     def format_condition(condition)
-        strings = condition.split("_")
-        return strings.join(" ")
+        strings = condition.split("_").map {|str|str.capitalize}
+        strings.join(" ")
     end
 end
